39da649b115ee8dec4c338d889ad83d74b60b259
Eli Felts <elifelts@gmail.com>
2025-04-27 07:36:38 -0700

    Just started with filtering by years on individual seasons

--- app.R
+++ app.R
@@ -4,7 +4,19 @@ library(tidyverse)
 library(googlesheets4)
 library(bslib)
 library(shiny)
+library(shinyWidgets)
 library(plotly)
+library(DT)
+library(conflicted)
+library(bsicons)
+library(fontawesome)
+library(arrow)
+
+
+conflicts_prefer(DT::renderDT,
+                 dplyr::filter,
+                 dplyr::lag,
+                 plotly::layout)
 
 
 # right now reading in from google sheets
@@ -13,3 +25,98 @@ library(plotly)
 
 matches_master <- read_sheet("https://docs.google.com/spreadsheets/d/1yDlDRlShRcc_aWd_SmDuJ-5naNwhjIQHPVN4UVcpM24/edit?gid=1555277773#gid=1555277773")
 wrestlers_master <- read_sheet("https://docs.google.com/spreadsheets/d/11TR6yUScjdF4OJYoqiVV4PqruJg2-KXmCCBljk9ABSw/edit?gid=325863048#gid=325863048")
+
+# start building individual page
+
+ind_years_formatted <- wrestlers_master %>% 
+  mutate(display_name=word(wrestler_id,1,sep="_",),
+         Record=str_c(wins,losses,sep="-"),
+         Matches=wins+losses,
+         `Bonus Percent`=round(bonus/Matches*100)) %>% 
+  select(Name = display_name,Team=team,Weight=weight_class,
+         Seed=seed,
+         Year=year,
+         Placement=placement,`Team Points`=team_points,
+         `Bonus Points`=bonus_points,Record,
+         Terminations=terminations,Pins=falls,
+         Bonus=bonus,
+         `Bonus Percent`,Matches,Wins=wins) 
+
+# make some formatting changes for individual tournaments
+
+
+# build user interface
+
+ui <- page_navbar(
+  
+  title="NCAA Wrestling Tournament Results",
+  
+  theme=bs_theme(preset="cyborg"),
+  
+  sidebar=sidebar(width=500,
+                  
+                  accordion(
+                    
+                    accordion_panel(
+                      
+                      "Explore Data",
+                      
+                      sliderInput(inputId = "ind_dates",
+                                  label="Choose a range of years",
+                                  min=min(wrestlers_master$year),
+                                  max=max(wrestlers_master$year),
+                                  value=c(1980,max(wrestlers_master$year)),
+                                  sep="")
+                      
+                    )
+                    
+                    )),
+  
+  nav_panel("Explore Data",
+
+            page_fillable(
+                      
+                      card(
+                        
+                        card_header("Individual Tournament Summaries"),
+                        DTOutput("ind_tourneys_table"),
+                        full_screen = TRUE
+                        
+                      )
+                      
+                    ))
+                    
+                  
+                  
+                  
+  
+)
+
+server <- function(input,output,session){
+  
+  # make the individual tournaments by wrestlers filter reactively
+  
+  ind_tourneys_reactive <- reactive({
+    
+    ind_tourney_min <- min(input$ind_dates)
+    ind_tourney_max <- max(input$ind_dates)
+    
+    ind_tourney.dat <- ind_years_formatted %>% 
+      filter(Year>=ind_tourney_min,
+             Year<=ind_tourney_max)
+    
+    
+  })
+  
+  # render a data table based on filters
+  
+  output$ind_tourneys_table <- renderDT({
+    
+    ind_tourneys_reactive() %>% 
+      arrange(desc(`Team Points`))
+    
+  })
+  
+}
+
+shinyApp(ui,server)
